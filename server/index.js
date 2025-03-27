9acd4857ad08db500b2d0de645d08168ac23f4e7
Dharajiya Yug Harshadbhai <yug@Dharajiyas-MacBook-Air.local>
2025-03-27 11:00:54 +0530

    bug fixes

--- server/index.js
+++ server/index.js
@@ -6,13 +6,6 @@ const path = require("path");
 const { Server } = require("socket.io");
 const { createServer } = require("http");
 
-
-const authRoutes = require("./routes/authRoutes");
-const productRoutes = require("./routes/productRoutes");
-const profileRoutes = require("./routes/profileRoutes");
-const chatRoutes = require("./routes/chatRoutes");
-
-
 // Initialize Express app
 const app = express();
 const httpServer = createServer(app); // Corrected
@@ -35,12 +28,10 @@ app.set("socketio", io);
 connectDB();
 
 // Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/user", profileRoutes);
-app.use("/api/chat", chatRoutes);
-
-
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/user", require("./routes/profileRoutes"));
+app.use("/api/chat", require("./routes/chatRoutes"));
 
 // Socket.io Logic
 io.on("connection", (socket) => {
@@ -69,20 +60,6 @@ io.on("connection", (socket) => {
         console.log("User disconnected:", socket.id);
     });
 });
-if (process.env.NODE_ENV === 'production') {
-  const distPath = path.join(__dirname, '../dist');
-  app.use(express.static(distPath));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(distPath, 'index.html'));
-  });
-
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("Something broke!");
-});
-
-}
 
 // Start Server
 const PORT = process.env.PORT || 5000;
